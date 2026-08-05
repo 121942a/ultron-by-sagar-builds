670eab8fa73d182c0c1e5feb8117dacccecddcfc
mehmetakif <mehmtakif20@gmail.com>
2026-08-05 13:31:55 +0300

    Capacitor Android ayarlari

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ultron.ai',
+  appName: 'Ultron AI',
+  webDir: 'out'
+};
+
+export default config;
